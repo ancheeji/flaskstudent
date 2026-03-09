d944d75d90a55d86a95aa0e02b37d802e4eb23ad
jm1021 <jmort1021@gmail.com>
2026-03-09 14:07:10 -0700

    titantic api

--- main.py
+++ main.py
@@ -26,6 +26,7 @@ from api.classroom_api import classroom_api
 from api.data_export_import_api import data_export_import_api
 from hacks.joke import joke_api  # Import the joke API blueprint
 from api.post import post_api  # Import the social media post API
+from api.titanic_api import titanic_api # Import the Titanic API blueprint
 #from api.announcement import announcement_api ##temporary revert
 
 # database Initialization functions
@@ -79,6 +80,7 @@ app.register_blueprint(feedback_api)
 app.register_blueprint(data_export_import_api)  # Register the data export/import API
 app.register_blueprint(joke_api)  # Register the joke API blueprint
 app.register_blueprint(post_api)  # Register the social media post API
+app.register_blueprint(titanic_api) # register api routes
 # app.register_blueprint(announcement_api) ##temporary revert
 
 # Jokes file initialization
